1b525c95c4a803f93daab7ad0e35987b7c9123e3
Thomas Gouveia <programmationthomas@gmail.com>
2020-12-11 12:27:22 +0100

    Fix chaining

--- lab2/index2.js
+++ lab2/index2.js
@@ -5,20 +5,18 @@ const util = require("util");
 const readFile = util.promisify(fs.readFile);
 
 const server = http.createServer((req, res) => {
+
   readFile("data.json")
-    .then((jsonData) => {
-      readFile("template.html")
-        .then((template) => {
-          const html = template.toString().replace(
-            "<li>%</li>",
-            JSON.parse(jsonData.toString())
-              .map((title) => `<li>${title}</li>`)
-              .join("\n")
-          );
-          res.end(html);
-        })
-        .catch(({ message }) => res.end(message));
-    })
+    .then((jsonData) => readFile("template.html")
+      .then((template) => {
+        const html = template.toString().replace(
+          "<li>%</li>",
+          JSON.parse(jsonData.toString())
+            .map((title) => `<li>${title}</li>`)
+            .join("\n")
+        );
+        res.end(html);
+      }))
     .catch(({ message }) => res.end(message));
 });
 
